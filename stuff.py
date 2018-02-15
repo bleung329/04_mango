f791476872cfe9c42235026c27c33b0c561b9ebf
vivienlee00 <vivien.lee.601@gmail.com>
2018-02-14 19:55:29 -0500

    done w required functions

--- stuff.py
+++ stuff.py
@@ -13,5 +13,17 @@ def find_by_zip(zip):
     stri = str(zip)
     return coll.find({'address.zipcode':stri})
 
-    
+def find_by_zipgrade(zip, grd):
+    strin = str(zip)
+    return coll.find( { "$and": [ {'address.zipcode':strin}, {'grade':grd}]})
+
+def find_by_zipscore(zip, scr):
+    st = str(zip)
+    scre = str(scr)
+    return coll.find( { "$and": [ {'address.zipcode':st}, {'score': {"$lt": scre}}]})
+
+
+print(find_by_borough('Brooklyn'))
 print(find_by_zip(10282))
+print(find_by_zipgrade(10282,"a"))
+print(find_by_zipgrade(10282,10))
